dead243822ae676b8354a080882f45a8bc234122
minimis <35778736+minimis@users.noreply.github.com>
2018-02-12 19:41:33 +0100

    Change01

--- JavaApplicationForGitMoj/src/javaapplicationforgitmoj/JavaApplicationForGitMoj.java
+++ JavaApplicationForGitMoj/src/javaapplicationforgitmoj/JavaApplicationForGitMoj.java
@@ -11,11 +11,9 @@ package javaapplicationforgitmoj;
  */
 public class JavaApplicationForGitMoj {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-      
+     
     }
     
 }
